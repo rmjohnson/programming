cec12ac067be758f0c0eb2baed169cf8b24c6369
Ryan Johnson <kritstov@gmail.com>
2011-08-06 17:33:38 -0700

    Added a few perl programs.

---
+++ perl/scalars.pl
@@ -0,0 +1,14 @@
+use strict;
+use warnings;
+
+my $string = "world";
+
+print "Hello ".$string."!\n";
+my $num1 = 1;
+my $num2 = 2;
+print $num1 + $num2 . "\n";
+
+# Numerical operators: <, >, <=, >=, ==, !=, <=>
+# String operators: lt, gt, le, ge, eq, ne, cmp
+
+# Perl has no boolean data type..lolwut
