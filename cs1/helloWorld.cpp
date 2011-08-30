eb277714a3dbe8433c373d5327a5b1348e1b31c6
Ryan Johnson <kritstov@gmail.com>
2011-08-30 09:37:20 -0500

    Added a folder for computer science 1 along with a hello world program.

---
+++ cs1/helloWorld.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	//Variables
+	string name; //The user's name
+
+	//Body of the program
+	cout << "Your name: ";
+	cin >> name;
+	cout << "Hello " << name << "!";
+	
+	return 0;
+	
+}
