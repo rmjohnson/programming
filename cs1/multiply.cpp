5c7db7aa2218d6224db4da7e985433041c5370da
Ryan Johnson <kritstov@gmail.com>
2011-08-30 13:11:07 -0500

    Made a multiplication program for cs1.

---
+++ cs1/multiply.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+int main() //START OF MAIN FUNCTION
+{
+	/////////////
+	//Variables//
+	/////////////
+	int num1,num2,result; //The first number to be multiplied, the second number to be multiplied, 
+								 //and the result of the multiplication.
+
+	////////////////////
+	//Body of program//
+	//////////////////
+	cout << "Please enter the first integer to be multiplied: "; //Prompt the user for the first number
+	cin >> num1; //Get input for first number
+
+	cout << "\nPlease enter the second integer to be multiplied: "; //Prompt the user for the second number
+	cin >> num2; //Get input for second number
+	
+	result = num1*num2 //Multiply the numbers together
+
+	cout << "\n" << num1 << " * " << num2 << " = " << result; //The final output in the form x * y = z
+
+	return 0;
+ 
+} //END OF MAIN FUNCTION
