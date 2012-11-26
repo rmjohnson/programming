960f6470a2c3b59c1bb262479be81f23c8f5938d
Ryan Johnson <kritstov@gmail.com>
2012-11-25 19:37:26 -0600

    add three musketeers sorting and data structures assignment 4.

--- NetBeansProjects/HashTable/src/hashtable/HashTable.java
+++ NetBeansProjects/HashTable/src/hashtable/HashTable.java
@@ -26,7 +26,7 @@ public class HashTable {
         for (double loadFactor = 1; loadFactor >= 0.5; loadFactor -= 0.05) {
             DecimalFormat df = new DecimalFormat("#.##");
             loadFactor = Double.parseDouble(df.format(loadFactor));
-            size = (int) (numwords * (1/loadFactor));
+            size = calcSize(numwords, loadFactor);
             f = new File("dictionary.txt");
             in = new Scanner(f);
             table = new String[size];
@@ -36,7 +36,6 @@ public class HashTable {
             for (int i = 0; i < numwords; i++) {
                 word = in.nextLine();
                 hashIndex = Math.abs(word.hashCode() % size);
-                //System.out.println(hashIndex);
                 while (table[hashIndex] != null) {
                     probes++;
                     if (hashIndex != size - 1) {
@@ -68,12 +67,33 @@ public class HashTable {
                     tmpHashIndex = (int) ((hashIndex + tmpProbes * tmpProbes) % size);
                 }
                 probes += tmpProbes;
-                //System.out.println(tmpHashIndex + " " + probes);
                 table[tmpHashIndex] = word;
             }
 
             System.out.println("Quadratic Load Factor: " + loadFactor);
             System.out.println("Quadratic Probes: " + probes);
         }
+    }
+    public static int calcSize(int size, double loadFactor) {
+        int tableSize = (int) (size * (1 / loadFactor));
+        while (!isPrime(tableSize) && tableSize < size) {
+            tableSize++;
+        }
+        return tableSize;
+    }
+
+    public static boolean isPrime(int n) {
+        if (n == 2) {
+            return true;
+        }
+        if (n % 2 == 0) {
+            return false;
+        }
+        for (int i = 3; i < n / 2; i++) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+        return true;
     }
 }
